f3a63d0056ab63566218bfbf33d8fef8cb0c4b3b
justin <justin.jahlas11@gmail.com>
2022-03-23 23:52:48 -0400

    gui working

--- ChatClientGUI.java
+++ ChatClientGUI.java
@@ -1,7 +1,10 @@
-
 import java.awt.*;
 import javax.swing.*;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.GroupLayout.Group;
+
 import java.awt.event.*;
+import java.util.Random;
 
 public class ChatClientGUI {
     
@@ -10,120 +13,264 @@ public class ChatClientGUI {
         JFrame frame = new JFrame(title);
 
         /* Hold all GUI elements for connect to host section */
-        JPanel connection_panel = new JPanel();
+        JPanel mainPanel = new JPanel();
+        JPanel connectionPanel = new JPanel();
+        JPanel searchPanel = new JPanel();
+        JPanel FTPPanel = new JPanel();
 
-        GroupLayout layout = new GroupLayout(connection_panel);
-        connection_panel.setLayout(layout);
-        layout.setAutoCreateGaps(true);
-        layout.setAutoCreateContainerGaps(true);
+        mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.Y_AXIS));
+
+        GroupLayout connectionLayout = new GroupLayout(connectionPanel);
+        GroupLayout searchLayout = new GroupLayout(searchPanel);
+        GroupLayout ftpLayout = new GroupLayout(FTPPanel);
         
-        /* Labels for host section*/
+        connectionPanel.setLayout(connectionLayout);
+        connectionPanel.setBorder(BorderFactory.createTitledBorder("Connection"));
+        connectionLayout.setAutoCreateGaps(true);
+        connectionLayout.setAutoCreateContainerGaps(true);
+
+        searchPanel.setLayout(searchLayout);
+        searchPanel.setBorder(BorderFactory.createTitledBorder("Search"));
+        searchLayout.setAutoCreateGaps(true);
+        searchLayout.setAutoCreateContainerGaps(true);
+
+        FTPPanel.setLayout(ftpLayout);
+        FTPPanel.setBorder(BorderFactory.createTitledBorder("FTP"));
+        ftpLayout.setAutoCreateGaps(true);
+        ftpLayout.setAutoCreateContainerGaps(true);
+
+        /* Labels */
+        // HOST SECTION
         JLabel serverHostNameLabel = new JLabel("Server Hostname");
         JLabel serverPortLabel = new JLabel("Port");
         JLabel serverUserNameLabel = new JLabel("Username");
         JLabel hostNameLabel = new JLabel("Hostname");
         JLabel speedLabel = new JLabel("Speed");
 
-        /* retrieves user input for host section fields */
-        JTextField serverHostName = new JTextField(16);
+        // SEARCH SECTION
+        JLabel keywordLabel = new JLabel("Keyword");
+
+        // FTP SECTION
+        JLabel commandLabel = new JLabel("Enter Command");
+
+        /* Text Fields */
+        // HOST SECTION
+        JTextField serverHostNameField = new JTextField(16);
         JTextField portField = new JTextField(7);
-        JTextField userName = new JTextField(16);
-        JTextField hostName = new JTextField(64);
+        JTextField userNameField = new JTextField(16);
+        JTextField hostNameField = new JTextField(64);
+
+        // SEARCH SECTION
+        JTextField keywordField = new JTextField(16);
+
+        // FTP SECTION
+        JTextField commandField = new JTextField(60);
+
+        JTextArea ftpTextArea = new JTextArea();        
         
-        String[] options = {"Ethernet", "Test"};
-        JComboBox<String> speed = new JComboBox<>(options);
+        /* Drop-Down menu */
+        // HOST SECTION
+        String[] options = {"Ethernet", "Modem", "T1", "T3"};
+        JComboBox<String> speedBox = new JComboBox<>(options);
         
-        /* Button that connects to server with user's given input */
-        JButton connect_button = new JButton("Connect");
-        connect_button.addActionListener(new ActionListener() {
+        /* Search Table */
+        String[] colNames = {"speed", "hostname", "filename"};
+        
+        String[][] data = {{"SPEED_1", "HOST1", "FILENAME1"}, {"SPEED_2", "HOST2", "FILENAME2"}};
+
+        JTable searchTable = new JTable(data, colNames);
+        searchTable.setPreferredScrollableViewportSize(searchTable.getPreferredSize());
+        JScrollPane tableScrollPane = new JScrollPane(searchTable);
+        JPanel tablePanel = new JPanel();
+        tablePanel.setLayout(new FlowLayout());
+        tablePanel.add(tableScrollPane);
+
+        /* Button */
+        JButton connectButton = new JButton("Connect");
+        JButton searchButton = new JButton("Search");
+        JButton ftpButton = new JButton("Go");
+
+        connectButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent aActionEvent) {
-                if (aActionEvent.getSource() == connect_button) {
+                if (aActionEvent.getSource() == connectButton) {
                     // TODO: add connect button pressed logic here
                     //////////////////////////////////////////////
                     System.out.println("Connect button pressed!");
 
                     /* Holds user's input*/
-                    String hostName = serverHostName.getText();
+                    /****************** USER VARIABLES  ***************************/
+                    String serverHostName = serverHostNameField.getText();
                     String port = portField.getText();
-                    System.out.println("Hostname: " + hostName + "\nPort: " + port);
+                    String userName = userNameField.getText();
+                    String hostName = hostNameField.getText();
+                    String speed = speedBox.getSelectedItem().toString();
+                    System.out.println("Hostname: " + serverHostName + "\nPort: " + port);
+                    System.out.println("User Name: " + userName + "\nHost Name: " + hostName);
+                    System.out.println("Speed: " + speed);
+                    /**************************************************************/
                 }
             }
         });
         
-        layout.setHorizontalGroup(
-            layout.createSequentialGroup()
-                
-            .addGroup(layout.createParallelGroup(GroupLayout.Alignment.LEADING)
+        searchButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent aActionEvent) {
+                if (aActionEvent.getSource() == searchButton) {
+                    // TODO: add connect button pressed logic here
+                    //////////////////////////////////////////////
+                    System.out.println("Search button pressed!");
+
+                    /* Holds user's input*/
+                    String keyword = keywordField.getText();
+                    System.out.println("Searching for: " + keyword);
+                }
+            }
+        });
+        
+        ftpButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent aActionEvent) {
+                if (aActionEvent.getSource() == ftpButton) {
+                    // TODO: add connect button pressed logic here
+                    //////////////////////////////////////////////
+                    System.out.println("Go button pressed!");
+
+                    /* Holds user's input*/
+                    ftpTextArea.append("appending testString " + new Random().nextInt() + "\n");
+                }
+            }
+        });
+
+        /* Set layout for page */
+        connectionLayout.setHorizontalGroup(
+            connectionLayout.createSequentialGroup()
+
+            // 1st col
+            .addGroup(connectionLayout.createParallelGroup(GroupLayout.Alignment.LEADING)
                 .addComponent(serverHostNameLabel)
                 .addComponent(serverUserNameLabel)
             )
-            .addGroup(layout.createParallelGroup(GroupLayout.Alignment.TRAILING)
-                .addComponent(serverHostName, 40, 300, 300)
-                .addComponent(userName, 40, 300, 300)
+
+            // 2nd col
+            .addGroup(connectionLayout.createParallelGroup(GroupLayout.Alignment.LEADING)
+                .addComponent(serverHostNameField, 40, 300, 300)
+                .addComponent(userNameField, 40, 300, 300)
             )
+
+            // 3rd col
             .addContainerGap(60, 60)
-            .addGroup(layout.createParallelGroup(GroupLayout.Alignment.TRAILING)
+
+            // 4th col
+            .addGroup(connectionLayout.createParallelGroup(GroupLayout.Alignment.LEADING)
                 .addComponent(serverPortLabel)
                 .addComponent(hostNameLabel)
             )
 
-            .addGroup(layout.createParallelGroup(GroupLayout.Alignment.TRAILING)
+            // 5th col
+            .addGroup(connectionLayout.createParallelGroup(GroupLayout.Alignment.LEADING)
                 .addComponent(portField, 40, 100, 100)
-                .addComponent(hostName, 40, 160, 160)
+                .addComponent(hostNameField, 40, 160, 160)
             )
+            // 6th col
             .addContainerGap(50, 50)
 
-            .addGroup(layout.createParallelGroup(GroupLayout.Alignment.LEADING)
-                .addComponent(connect_button, 200, 200, 200)
-
-                .addGroup(layout.createSequentialGroup()
+            // 7th col
+            .addGroup(connectionLayout.createParallelGroup(GroupLayout.Alignment.LEADING)
+                .addComponent(connectButton, 150, 150, 150)
+                .addGroup(connectionLayout.createSequentialGroup()
                     .addComponent(speedLabel)
-                    .addComponent(speed, 40, 100, 100)
+                    .addComponent(speedBox, 40, 100, 100)
                 )
-                
             )
-                
         );
-
-        layout.setVerticalGroup(
-            layout.createSequentialGroup()
-            .addGroup(layout.createParallelGroup(GroupLayout.Alignment.BASELINE)
+        /* set vertical layout for page */
+        connectionLayout.setVerticalGroup(
+            connectionLayout.createSequentialGroup()
+            // 1st row
+            .addGroup(connectionLayout.createParallelGroup(GroupLayout.Alignment.LEADING)
                 .addComponent(serverHostNameLabel)
-                .addComponent(serverHostName)
+                .addComponent(serverHostNameField)
                 .addComponent(serverPortLabel)
                 .addComponent(portField)
-                .addComponent(connect_button)
+                .addComponent(connectButton)
             )
-            .addGroup(layout.createParallelGroup(GroupLayout.Alignment.BASELINE)
+            // 2nd row
+            .addGroup(connectionLayout.createParallelGroup(GroupLayout.Alignment.LEADING)
                 .addComponent(serverUserNameLabel)
-                .addComponent(userName)
+                .addComponent(userNameField)
                 .addComponent(hostNameLabel)
-                .addComponent(hostName)
+                .addComponent(hostNameField)
                 .addComponent(speedLabel)
-                .addComponent(speed)
+                .addComponent(speedBox)
             )
         );
 
+        /** SEARCH FORMAT */
+        searchLayout.setHorizontalGroup(
+            searchLayout.createSequentialGroup()
+            .addGroup(searchLayout.createParallelGroup(GroupLayout.Alignment.LEADING)
+                .addGroup(searchLayout.createSequentialGroup()
+                    .addComponent(keywordLabel)
+                    .addComponent(keywordField, 100, 300, 300)
+                    .addComponent(searchButton)
+                )
+            .addComponent(tablePanel)
+            )
+        );
 
-        /* add all previous fields to panel */
-        // Host name
-        // connection_panel.add(serverHostNameLabel);
-        // connection_panel.add(serverHostName);
+        searchLayout.setVerticalGroup(
+            searchLayout.createSequentialGroup()
+            .addGroup(searchLayout.createParallelGroup(GroupLayout.Alignment.LEADING)
+                .addComponent(keywordLabel)
+                .addComponent(keywordField)
+                .addComponent(searchButton)
+            )
+            .addContainerGap(50, 50)
+            .addComponent(tablePanel)
+        );
 
-        // // port
-        // connection_panel.add(serverPortLabel);
-        // connection_panel.add(portField);
+        ftpLayout.setHorizontalGroup(
+            ftpLayout.createSequentialGroup()
+            .addGroup(ftpLayout.createParallelGroup(GroupLayout.Alignment.LEADING)
+                .addGroup(ftpLayout.createSequentialGroup()
+                    .addComponent(commandLabel)
+                    .addComponent(commandField, 200, 200, 200)
+                    .addComponent(ftpButton)
+                )
+                .addComponent(ftpTextArea, 500, 500, 500)
+            )
+            
+        );
 
-        // // button
-        // connection_panel.add(connect_button);
+        ftpLayout.setVerticalGroup(
+            ftpLayout.createSequentialGroup()
+            .addGroup(ftpLayout.createParallelGroup(GroupLayout.Alignment.LEADING)
+                .addComponent(commandLabel)
+                .addComponent(commandField)
+                .addComponent(ftpButton)
+            )
+            .addComponent(ftpTextArea)
+        );
 
         /** finalize frame settings **/
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(1000, 600);
+        
+        mainPanel.add(connectionPanel);
+        mainPanel.add(searchPanel);
+        mainPanel.add(FTPPanel);
+
+        JScrollPane mainPanelScroll = new JScrollPane(
+            mainPanel,
+            JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, 
+            JScrollPane.HORIZONTAL_SCROLLBAR_NEVER
+        );
+        // faster scroll speed
+        mainPanelScroll.getVerticalScrollBar().setUnitIncrement(16);
 
         // add connection panel to frame
-        frame.getContentPane().add(connection_panel);
+        frame.getContentPane().add(mainPanelScroll);
 
         // center frame to screen
         frame.setLocationRelativeTo(null);
@@ -133,6 +280,6 @@ public class ChatClientGUI {
     }
 
     public static void main(String[] args) {
-        ChatClientGUI cgui = new ChatClientGUI("Test");
+        ChatClientGUI cgui = new ChatClientGUI("GV-NAPSTER Host");
     }
 }
